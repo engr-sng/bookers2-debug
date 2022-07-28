ecdc521fddafa05bb00088b6ab3cc53ab31ca004
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-07-28 09:20:58 +0000

    [finish]課題5【Bookers2へ「検索機能」を実装】

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,24 @@
+class SearchesController < ApplicationController
+
+  def search
+    @model_selection = params[:model_selection]
+    @retrieval_method = params[:retrieval_method]
+
+    if @retrieval_method == "perfect_match"
+      @word = "#{params[:word]}"
+    elsif @retrieval_method == "forward_match"
+      @word = "#{params[:word]}%"
+    elsif @retrieval_method == "backward_match"
+      @word = "%#{params[:word]}"
+    elsif @retrieval_method == "partial_match"
+      @word = "%#{params[:word]}%"
+    end
+
+    if @model_selection == "User"
+      @users = User.where("name LIKE ?", "#{@word}").or(User.where("introduction LIKE ?", "#{@word}"))
+    else
+      @books = Book.where("title LIKE ?", "#{@word}").or(Book.where("body LIKE ?", "#{@word}"))
+    end
+  end
+
+end
