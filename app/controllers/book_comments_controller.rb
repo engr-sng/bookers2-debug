f1a768a635ed758a350d32773f963150aa550d74
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-07-29 09:30:44 +0000

    [finish]課題6【課題3で実装した機能の非同期通信化】

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,26 @@
 class BookCommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
-  
+
   def create
     book = Book.find(params[:book_id])
     book_comment = BookComment.new(book_comment_params)
     book_comment.user_id = current_user.id
     book_comment.book_id = book.id
     book_comment.save
-    redirect_back fallback_location: books_path
+    # redirect_back fallback_location: books_path
+    # render :book_comments_index
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
   end
 
   def destroy
-    book_comment = BookComment.find(params[:id])  
+    book_comment = BookComment.find(params[:id])
     book_comment.destroy
-    redirect_back fallback_location: books_path
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
+    # redirect_back fallback_location: books_path
+    # render :destroy
+
   end
 
   def ensure_correct_user
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,18 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     favorite = Favorite.new
     favorite.user_id = current_user.id
-    favorite.book_id = book.id
+    favorite.book_id = @book.id
     favorite.save
-    redirect_back fallback_location: books_path
+    # redirect_back fallback_location: books_path
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = Favorite.find_by(user_id: current_user.id, book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
     favorite.destroy
-    redirect_back fallback_location: books_path
+    # redirect_back fallback_location: books_path
   end
 end
